ea22a205384c53b18b378ce2bc7d3045cecde6b0
Kleiton Homem Martins <kleitonhomemmartins@rede.ulbra.br>
2024-10-15 21:25:23 -0300

    small fix

--- ap2/src/pedidos/Fila.java
+++ ap2/src/pedidos/Fila.java
@@ -7,63 +7,6 @@ package pedidos;
 //Imprimir Pedidos Cancelados: Imprima todos os pedidos presentes na pilha de pedidos cancelados, exibindo seu ID e descrição.
 
 public class Fila {
-    private Object elementos;
-    private int tamanho;
 
-    public Fila (int capacidade){
-        this.elementos = new String[capacidade];
-        this.tamanho = 0;
-    }
-
-    public int tamanho(){
-        return this.tamanho;
-    }
-
-    public boolean estaVazia(){
-        return tamanho == 0;
-    }
-
-    public boolean estaCheia(){
-        return this.tamanho == this.elementos.length;
-    }
-
-    public boolean enfileirar(String e){
-        if(!estaCheia()){
-            this.elementos[tamanho] = e;
-            tamanho ++;
-            return true;
-        }
-        return false;
-    }
-
-    public String espiar(){
-        if (!estaVazia()){
-            return this.elementos[0];
-        }
-        return null;
-    }
-    public String desenfileirar(){
-        if (!estaVazia()){
-            String removido = this.elementos[0];
-            for (int i = 1; i < tamanho; i++){
-                this.elementos[i - 1] = this.elementos[i];
-            }
-            tamanho --;
-            return removido;
-        }
-        return null;
-    }
-    @Override
-    public String toString(){
-        String s = "[";
-        for (int i = 0; i < tamanho; i++){
-            s += elementos[i];
-            if (i < tamanho - 1){
-                s += ",";
-            }
-        }
-        s += "]";
-        return s;
-    }
 }
 
